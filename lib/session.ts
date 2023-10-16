3c4fda9c6902a2930583dd415b89e65eb2569319
Govinda Hosein <gh604721@digicelgroup.com>
2023-10-16 15:00:05 -0400

    Partial implement edit project

--- lib/session.ts
+++ lib/session.ts
@@ -81,7 +81,7 @@ export const authOptions: NextAuthOptions = {
 };
 
 export async function getCurrentUser() {
-  const session = (await getServerSession(authOptions)) as SessionInterface;
+  const session = await getServerSession(authOptions);
 
-  return session;
+  return session as SessionInterface;
 }
