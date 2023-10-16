4152afc6153026e5abe1532573215ed656c50347
Govinda Hosein <gh604721@digicelgroup.com>
2023-10-16 12:39:08 -0400

    Implement delete project

--- components/ProjectActions.tsx
+++ components/ProjectActions.tsx
@@ -2,10 +2,26 @@
 import Link from "next/link";
 import Image from "next/image";
 import React, { useState } from "react";
+import { deleteProject, fetchToken } from "@/lib/actions";
+import { useRouter } from "next/navigation";
 
 const ProjectActions = ({ projectId }: { projectId: string }) => {
+  const router = useRouter();
   const [isDeleting, setIsDeleting] = useState(false);
-  const handleDeleteProject = async () => {};
+  const handleDeleteProject = async () => {
+    setIsDeleting(true);
+
+    const { token } = await fetchToken();
+
+    try {
+      await deleteProject(projectId, token);
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsDeleting(false);
+    }
+  };
 
   return (
     <>
@@ -16,7 +32,13 @@ const ProjectActions = ({ projectId }: { projectId: string }) => {
         <Image src="/pencile.svg" width={15} height={15} alt="edit" />
       </Link>
 
-      <button type="button" className={`flexCenter delete-action_btn bg-gray`}>
+      <button
+        type="button"
+        className={`flexCenter delete-action_btn  ${
+          isDeleting ? "bg-gray" : "bg-primary-purple"
+        }`}
+        onClick={handleDeleteProject}
+      >
         <Image src="/trash.svg" width={15} height={15} alt="delete" />
       </button>
     </>
